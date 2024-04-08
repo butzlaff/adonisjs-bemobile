d005229b02616a9a434a9b053cbebe9f21743612
butzlaff <emiliobutz@gmail.com>
2024-04-08 16:12:47 -0300

    feat: address table

---
+++ app/models/address.ts
@@ -0,0 +1,22 @@
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon'
+
+export default class Address extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare street: string
+
+  @column()
+  declare district: string
+
+  @column()
+  declare adressNumber: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ database/migrations/1712603340259_create_addresses_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'addresses'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('street')
+      table.string('district')
+      table.integer('address_number')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
