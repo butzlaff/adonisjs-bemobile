f5f85d712b35c9b04be9dd0c1c00d12327ba1262
butzlaff <emiliobutz@gmail.com>
2024-04-08 15:47:49 -0300

    feat: create users table

---
+++ app/controllers/users_controller.ts
@@ -0,0 +1,38 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class UsersController {
+  /**
+   * Display a list of resource
+   */
+  async index({}: HttpContext) {
+    return 'ok'
+  }
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {
+    return { request }
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {
+    return { params }
+  }
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {
+    return { params, request }
+  }
+
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {
+    return { params }
+  }
+}
