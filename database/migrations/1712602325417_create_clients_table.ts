16fad1a506c831830aea65e0d2e1003e25ae5c1b
butzlaff <emiliobutz@gmail.com>
2024-04-12 20:33:55 -0300

    refact: make changes in migrations

--- database/migrations/1712602325417_create_clients_table.ts
+++ database/migrations/1712602325417_create_clients_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
       table.string('name').notNullable()
-      table.string('cpf').notNullable()
+      table.string('cpf').notNullable().unique().checkLength('=', 11)
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').notNullable()
     })
--- database/migrations/1712607654242_create_sales_table.ts
+++ database/migrations/1712607654242_create_sales_table.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.decimal('total_price').unsigned()
       table
         .integer('client_id')
         .notNullable()
