64f89cb9137e87bf2ec98b87ba548ad591ac390c
butzlaff <emiliobutz@gmail.com>
2024-05-07 03:58:47 -0300

    fix: fix User list test

--- tests/functional/users/list.spec.ts
+++ tests/functional/users/list.spec.ts
@@ -2,8 +2,16 @@ import { test } from '@japa/runner'
 
 test.group('Users list', () => {
   test('Cannot List users without authenticate', async ({ client }) => {
-    const response = await client.get('/users')
+    const login = await client.post('/login').json({
+      email: 'user@test.com',
+      password: '123456',
+    })
+    const { token } = login.body()
 
-    response.assertStatus(401)
+    const response = await client.get('/users').bearerToken(token)
+
+    response.assertStatus(200)
+    response.assertBody([{ id: 1, email: 'user@test.com' }])
+    response.assertBodyNotContains({ id: 2, email: 'invaliduserteste.com' })
   })
 })
